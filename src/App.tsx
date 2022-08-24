6cb330a7edcff4322450c0b7c339d13e32b2808a
w6fux5 <w6fux5@gmail.com>
2022-08-24 11:05:40 +0800

    chore: project init and eslint prettier setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Cool } from '@/features/cool';
+
+export const App = () => (
+  <div>
+    <Cool />
+  </div>
+);
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import {resolve} from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': resolve(__dirname, './src'),
+    }
+  },
+});
